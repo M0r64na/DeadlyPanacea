ff03d8cf34df89edc641d70d83d65e1e7e87662b
Kiara Lazarova <kival1020@yahoo.com>
2022-03-06 02:45:04 +0200

    Added graphics in menu state

--- src/js/menuState.js
+++ src/js/menuState.js
@@ -1,13 +1,21 @@
 var menuState = {
     create: function () {
-        startButton = game.add.button(346, 407, 'startButton', () => { game.state.start('world'); });
-		startButton.scale.setTo(0.84, 0.69);
-
-        // TO DO - add how-to-play button
-        // howToPlayButton = game.add.button(x1, y1, 'howToPlayButton', () => { game.state.start('guide'); });
-        // startButton.scale.setTo(x2, y2);
-
         startMenuBackground = game.add.image(0, 0, 'startMenuBackground');
-        startMenuBackground.scale.setTo(width / 1620, height / 894);
+        startMenuBackground.scale.setTo(width / 1920, height / 1080);
+
+        title = game.add.image(width / 10, 50, 'title');
+        title.scale.setTo(0.5, 0.5);
+
+        startButton = game.add.button(width / 3, height / 3 + 60, 'startButton', () => { game.state.start('world'); });
+		startButton.scale.setTo(0.7, 0.7);
+
+        guideButton = game.add.button(width / 3, height / 2 + 70, 'guideButton', () => { game.state.start('guide'); });
+        guideButton.scale.setTo(0.7, 0.7);
+
+        firstTestTubeSet = game.add.image(5, 70, 'first_test_tube_set');
+        firstTestTubeSet.scale.setTo(0.23, 0.23);
+
+        secondTestTubeSet = game.add.image(width * 4.8 / 7, 70, 'second_test_tube_set');
+        secondTestTubeSet.scale.setTo(0.23, 0.23);
     }
 }
